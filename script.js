7ec97dadc7badbe27e66eb7d762793b84ffd0097
Breno Vasc <beniolimavasc@gmail.com>
2023-04-28 00:11:22 -0300

    att

--- script.js
+++ script.js
@@ -14,32 +14,21 @@ new TypeIt(".auto-text", {
 
 
 
-const body = document.body;
-let lastScroll = 0;
 
-window.addEventListener("scroll", () => {
-	const currentScroll = window.pageYOffset;
-	if (currentScroll <= 0) {
-		body.classList.remove("scroll-up");
-		return;
-	}
-
-	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-		body.classList.remove("scroll-up");
-		body.classList.add("scroll-down");
-	} else if (
-		currentScroll < lastScroll &&
-		body.classList.contains("scroll-down")
-	) {
-		body.classList.remove("scroll-down");
-		body.classList.add("scroll-up");
-	}
-	lastScroll = currentScroll;
-});
 
 
 window.sr = ScrollReveal({reset: true});
+const icon = document.querySelector('.back-to-top');
 
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 600) {
+    icon.classList.remove('disabled');
+  } else {
+    icon.classList.add('disabled');
+  }
+});
 
 
 ScrollReveal().reveal('.me, .me-p, .myskills, .tecnologias', {delay: 600});
